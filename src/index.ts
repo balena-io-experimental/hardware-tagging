bfea57fdc0f5ad10aee33a15c2802076dc30e433
Matthew McGinn <matthew@balena.io>
2020-04-17 15:59:12 -0400

    add tag for disk size
    Signed-off-by: Matthew McGinn <matthew@balena.io>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import * as os from "os";
 import * as _ from "lodash";
 import * as balena from "balena-sdk";
@@ -10,8 +11,8 @@ const BALENA_DEVICE_UUID = process.env.BALENA_DEVICE_UUID ?? "";
 const getMacAddresses = () => {
 	return _.compact(
 		_.map(os.networkInterfaces(), (obj, iface) => {
-			if (iface.startsWith('resin-')) {
-				return null
+			if (iface.startsWith("resin-")) {
+				return null;
 			}
 			return obj[0].internal ? null : [iface, obj[0].mac];
 		})
@@ -22,6 +23,26 @@ const getCpu = () => {
 	return os.cpus()[0].model;
 };
 
+const getDiskSize = () => {
+	const root = "/sys/block";
+	const stat = fs.readdirSync(root);
+	return _.compact(
+		_.map(stat, blkdev => {
+			const majMin = fs.readFileSync(`${root}/${blkdev}/dev`, "utf8").trim();
+			// ignore loopbacks and ramdisks, and only care about minor devices :0 (ze root)
+			if (!majMin.endsWith(":0")) {
+				return null;
+			}
+			if (majMin.startsWith("7:") || majMin.startsWith("1:")) {
+				return null;
+			}
+			const size = (((fs.readFileSync(`${root}/${blkdev}/size`, "utf8") as unknown) as number) * 512) / 1024 ** 3;
+			console.log(`blkdev: ${blkdev}, size: ${size}`);
+			return size > 0 ? [`${blkdev.toUpperCase()}_SIZE_GB`, `${size}`] : null;
+		})
+	);
+};
+
 const setTag = async (key: string, val: string) => {
 	console.log(`setting ${key} to ${val}...`);
 	await sdk.models.device.tags.set(BALENA_DEVICE_UUID, key, val);
@@ -30,9 +51,11 @@ const setTag = async (key: string, val: string) => {
 const getHardwareTags = async () => {
 	const tags = [
 		["CPU", getCpu()],
-		["TotalMemMb", `${os.totalmem() / 1024.0 ** 2}`],
-		["KernelRelease", os.release()]
-	].concat(getMacAddresses());
+		["TOTAL_MEM_MB", `${os.totalmem() / 1024.0 ** 2}`],
+		["KERNEL_RELEASE", os.release()],
+		...getMacAddresses(),
+		...getDiskSize()
+	];
 	_.forEach(tags, async item => {
 		await setTag(item[0], item[1]);
 	});
